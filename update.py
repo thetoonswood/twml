5d9cc49090a6e3d1d4844797e637a3eb15b193dd
thetoonswood <118585979+thetoonswood@users.noreply.github.com>
2022-12-15 18:08:09 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/weebzone/WZML"
+    UPSTREAM_REPO = "https://github.com/thetoonswood/twml"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
